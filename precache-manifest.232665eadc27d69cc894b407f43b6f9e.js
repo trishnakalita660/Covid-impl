ccabca9f0f3e3f79435cd4367922048c61a2fefc
trishnakalita660 <trishnakalita660@gmail.com>
2021-03-06 19:30:54 +0530

    Updates

---
+++ precache-manifest.232665eadc27d69cc894b407f43b6f9e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6574086aa8d17be32a91938f4b8847e",
+    "url": "/Covid-impl/index.html"
+  },
+  {
+    "revision": "735e396b3bec135f9cc9",
+    "url": "/Covid-impl/static/css/main.f955c4b9.chunk.css"
+  },
+  {
+    "revision": "027c3a9f4aee0fa86efc",
+    "url": "/Covid-impl/static/js/2.59f50938.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/Covid-impl/static/js/2.59f50938.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "735e396b3bec135f9cc9",
+    "url": "/Covid-impl/static/js/main.b402951f.chunk.js"
+  },
+  {
+    "revision": "6c498c9a3a19f4649161",
+    "url": "/Covid-impl/static/js/runtime-main.0a60ac3f.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/Covid-impl/static/media/image.d7265326.png"
+  }
+]);
